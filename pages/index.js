a0ac1c0728aeaae482885dcb0e8481bd1aa3d4a9
minki607 <suh@student.unimelb.edu.au>
2021-12-01 17:49:57 +0900

    feat: single page routing (getStaticProps , getStaticPathas)

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,9 @@ export default function Home({ posts }) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch("http://localhost:8080/api/posts");
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_start=0&_end=10"
+  );
   const posts = await res.json();
 
   return {
